76737902756c195dd23d2353d039f54dc91f11eb
Faysal2123 <foysal2005n1@gmail.com>
2024-12-04 22:45:30 +0600

    register section done

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="flex justify-center ">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-20">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-6 mb-6 lg:mt-20">
         <form className="card-body">
           <div className="form-control">
             <label className="label">
--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -1,12 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div className="flex justify-center ">
-     
+  return (
+    <div className="flex justify-center ">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-5 mb-5">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter Your Name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter Your Email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo</span>
+            </label>
+            <input
+              name="photo"
+              type="text"
+              placeholder="Photo Url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-error">Register</button>
+          </div>
+          <div>
+            <p>
+              Already have an account{" "}
+              <Link to="/login" className="font-semibold text-blue-600">
+                Login
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Register;
