2f2b5275116f4c9c3b0d768805722a95d0afaff6
Faysal2123 <foysal2005n1@gmail.com>
2024-12-04 16:28:55 +0600

    footer section done

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -99,20 +99,7 @@ const Navbar = () => {
         <div className="navbar-end">
           <a className="btn btn-accent font-bold">
             Login
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 ml-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h14M12 5l7 7-7 7"
-              />
-            </svg>
+          
           </a>
         </div>
       </div>
